7d1d16fe399de841102e42a70bcb4a0da7e3a8ee
Zephraph <zephraph@gmail.com>
2017-05-10 23:54:49 -0400

    Update deps, add linter, fix lint errors

--- src/io.ts
+++ src/io.ts
@@ -1,17 +1,19 @@
 import { readFile as read, writeFile as write } from 'fs';
 
-export const readFile = (file: string) =>
+export const readFile = (file: string): Promise<string> =>
   new Promise((resolve, reject) => {
     read(file, 'utf8', (err, contents) => {
-      if (err) reject(err);
-      resolve(contents);
+      err
+        ? reject(err)
+        : resolve(contents);
     });
   });
 
 export const writeFile = (file: string, content: string) =>
   new Promise((resolve, reject) => {
     write(file, content, err => {
-      if (err) reject(err);
-      resolve();
+      err
+        ? reject(err)
+        : resolve();
     });
-  })
+  });
